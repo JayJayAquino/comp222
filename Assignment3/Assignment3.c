7282b114adbcb234facfa1b0609a390b6fb89f2f
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-10-29 13:51:06 -0700

    finish project

--- Assignment3/Assignment3.c
+++ Assignment3/Assignment3.c
@@ -27,55 +27,51 @@ void set_params()
 /***************************************************************/
 void check_hamming()
 {
-    /* declare local vars */
-    int actual_length, parity_check, hamming_var, i, j, k;
-    int error_bit = 0;
+  /* declare local vars */
+  int actual_length;
+  int i,j,k;
+  int parity_check;
+  int starting_point;
+  int error_bit = 0;
 
-    /* prompt for hamming code as a "string"*/
-    printf("Enter the Hamming code: ");
-    scanf("%s", hamming_string);
-
-
-    actual_length = strlen(hamming_string);
-    num_parity = ceil(log(actual_length) / log(2));
-
-     /* OUTER LOOP: for each parity bit in the Hamming code*/
-    for (i = 0; i < num_parity; i++)
-    {
-
-        hamming_var = pow(2,i);
-        parity_check = hamming_parity;
-
-        /* MIDDLE LOOP: for each starting bit of a sequence */
-        for (j = i; j < actual_length; j = j +(2 * hamming_var)) {
-
-            /* INNER LOOP: for each bit to be checked, check value and update parity information for current parity bit*/
-            for (k = j; (k < ((2 * j) - 1)) && (k <= actual_length);  k++) {
-                parity_check ^= (hamming_string[actual_length - k] - '0');
-            } // end k
-        } // end j
-
-        error_bit = error_bit + (parity_check * hamming_var);
-    } // end i
+  /* prompt for hamming code as a "string"*/
+  printf("Enter the Hamming code: ");
+  scanf("%s", hamming_string);
 
 
+  actual_length = strlen(hamming_string);
+  num_parity = ceil(log(actual_length) / log(2));
 
-    if (error_bit == 0) {
-        printf("There is no bit error");
+   /* OUTER LOOP: for each parity bit in the Hamming code*/
+  for (i = 1 ; i <= actual_length ; i = i * 2){
+      // starting_point = pow(2,i); not necessary
+      parity_check = hamming_parity;
+
+      /* MIDDLE LOOP: for each starting bit of a sequence */
+      for (j = i; j <= actual_length; j = j + (i*2)){
+
+          /* INNER LOOP: for each bit to be checked, check value and update parity information for current parity bit*/
+          for (k = j; (k < (i+j)) && (k <= actual_length);  k++){
+              parity_check ^= (hamming_string[actual_length - k] - '0');
+          } // end k
+      } // end j
+
+      error_bit = error_bit + (parity_check * i);
+  } // end i
+
+
+
+  if (error_bit == 0) {
+    printf("\nThere is no bit error\n\n");
+  }else{
+    printf("\nThere is an error in bit: %d\n", error_bit);
+    if (hamming_string[actual_length - error_bit] == '1') {
+        hamming_string[actual_length - error_bit] = '0';
+    }else{
+        hamming_string[actual_length - error_bit] = '1';
     }
-
-    else {
-        printf("There is an error in bit: %d\n", error_bit);
-        if (hamming_string[actual_length - error_bit] == '0') {
-            hamming_string[actual_length - error_bit] = '1';
-        }
-        else {
-            hamming_string[actual_length - error_bit] = '0';
-
-        } // end else
-        printf("The corrected Hamming code is: %s\n", hamming_string);
-
-    }
+    printf("The corrected Hamming code is: %s\n\n", hamming_string);
+  }
 
 return;
 }
